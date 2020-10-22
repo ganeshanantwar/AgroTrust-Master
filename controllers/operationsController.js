cbeabc7ac13e3077b5a972723c952e2b98474e60
Ganesh <ganesh@emertech.io>
2020-10-23 02:26:55 +0530

    Business logic for any inward, work in progress

--- controllers/operationsController.js
+++ controllers/operationsController.js
@@ -1,53 +1,96 @@
 const multichain = require('../utils/multichain.js');
-
+const streamer = require('../utils/streamer');
 
 exports.processInward = async (req, res) => {
+	//validate mandatory fields
+	let LECode = req.params.le;
+	let materialCode = req.body.materialCode;
+	let fromLoc = req.body.fromLoc;
+	let toLoc = req.body.toLoc;
+	let skuCode = req.body.skuCode;
+	let skuQuantity = req.body.skuQuantity;
+	let totalWeight = req.body.totalWeight;
+	let price = req.body.price;
+	let rejection = req.body.rejection;
+	let date = req.body.date;
+	let transferCost = req.body.transferCost;
+	let locationCost = req.body.locationCost;
 
-    //parse request JSON for mandatory and optional fields
-    let fromLoc = req.body.fromLoc;
-    let toLoc = req.body.toLoc;
-    let toLE = req.body.toLE;
-    let date = req.body.date;
-    let orderID = req.body.orderID;
-    let delNote = req.body.delNote;
-    let cropID = req.body.cropID;
-    let varietyID = req.body.varietyID;
-    let skuCode = req.body.skuCode;
-    let skuQuantity = req.body.skuQuantity;
-    let uomPrice = req.body.uomPrice;
-    let transferCost = req.body.transferCost;
+	//get SKU object
+	let crops = [];
+	crops.push(materialCode.toString().substr(0, 4));
+	let inSKU = await streamer.fetchOne('sku', skuCode, req.params.le, crops);
 
+	//validate if difference between received total weight and skuQuantity*inSKU.nWeight is within 10KG
+	if (Math.abs(totalWeight - skuQuantity * inSKU.nWeight) >= 10) {
+		return res.status(400).json({
+			success: false,
+			message:
+				'Total Weight for ' +
+				skuQuantity +
+				' SKUs should be ' +
+				skuQuantity * inSKU.nWeight +
+				' but actual weight is ' +
+				totalWeight +
+				'. Please check weighment!',
+		});
+	} else {
+		//adjust total weight with rejections
+		totalWeight -= rejection;
 
-    if (fromLoc.substring(0, 1) == 'e') {
+		//check if from location is a plot, if yes set the origin flag
+		let isOrigin = req.body.fromLoc.toString().substr(0, 2) == 'aa';
+		if (isOrigin) {
+			//get next BTU code
+			btuCode = await streamer.nextCode('btu', LECode, crops);
+			//create a transfer object
+			/*let inward_transfer={
+                fromLoc:
+                toLoc:
+                date:
+                locationCost:
+                transferCost:
+                buyerLE:
+                sellerLE:
+                price:
+            }*/
 
-        //Check if harvest batch for this field already exists
-        let allBTUs=await multichain(toLE, [cropID], 'liststreamitems', ['btu']);
-        allBTUNames=allBTUs
+			//create a BTU object at plot
+			let btu = {
+				btuCode: btuCode,
+				btuName: 'inward-batch',
+				materialCode: materialCode,
+				skuCode: skuCode,
+				skuQuantity: skuQuantity,
+				isFinished: inSKU.isFinished,
+				origin: fromLoc,
+				oneDown: null,
+				downMixed: false,
+				oneUp: null,
+				upMixed: false,
+				transformDate: date,
+				transformName: 'HARVEST',
+				totalWeight: totalWeight,
+				materialCost: totalWeight * price,
+				transformCost: 0,
+				totalValue: totalWeight * price + locationCost + transferCost,
+				transfers: [],
+			};
+		} else {
+		}
+	}
+};
 
+exports.processConvert = async (req, res) => {
+	return res.status(200).json({
+		success: false,
+		message: 'TO BE IMPLEMENTED',
+	});
+};
 
-        //Generate harvest-batch BTU
-        let btuIDs = await multichain(toLE, [cropID], 'liststreamkeyitems', ['idmap', 'btu', false, 1, -1]);
-        let btuID1 = (parseInt('0x' + tf1IDs[0][0].data) + 1).toString(16);
-        let btuID2 = (parseInt('0x' + tf1IDs[0][0].data) + 2).toString(16);
-
-
-        
-
-        //Record a transfer from fromLoc to toLoc
-        let tr1IDs = await multichain(toLE, [cropID], 'liststreamkeyitems', ['idmap', 'transfer', false, 1, -1]);
-        let tr1ID = (parseInt('0x' + tr1IDs[0][0].data) + 1).toString(16);
-        let tr1={
-            transferID:tr1ID,
-            type:'Purchase',
-
-        }
-
-        //Generate a BTU 
-
-
-    } else {
-
-    }
-
-
-}
+exports.processDispatch = async (req, res) => {
+	return res.status(200).json({
+		success: false,
+		message: 'TO BE IMPLEMENTED',
+	});
+};
